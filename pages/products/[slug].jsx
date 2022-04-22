507cd5c92964fbcb410da135954ceb1f7ecc7bf0
Jono Warren <jono@gener8ads.com>
2022-04-22 17:48:52 +0100

    Dynamic path

---
+++ pages/products/[slug].jsx
@@ -0,0 +1,33 @@
+import { useMemo } from 'react';
+import useSWR from 'swr';
+import Layout from '../../components/Layout'
+import { useRouter } from 'next/router'
+
+const fetchJson = (...args) => fetch(...args).then(res => res.json());
+
+export default function Product() {
+  const router = useRouter()
+  const slug = router.query.slug || '';
+
+  const { data, error } = useSWR(`https://apollo.gener8.tech/marketplace/products?slug=${slug}`, fetchJson);
+
+  const product = useMemo(() => {
+    if (!data) {
+      return;
+    }
+
+    return data.data[0];
+  }, [data]);
+
+  console.log(product);
+
+  return (
+    <Layout>
+      {product && (
+        <div>
+          {product.attributes.title}
+        </div>
+      )}
+    </Layout>
+  )
+}
