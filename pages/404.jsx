5b6ba258a457cfdd7e055973eeb6c2035f1b1d77
Jono Warren <jono@gener8ads.com>
2022-04-22 17:56:13 +0100

    Add 404 page

---
+++ pages/404.jsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Layout from "../components/Layout";
+
+export default function NotFound() {
+  return (
+    <Layout>
+      <Head>
+        <title>Page not found</title>
+      </Head>
+
+      <h1>Page not found</h1>
+    </Layout>
+  )
+}
